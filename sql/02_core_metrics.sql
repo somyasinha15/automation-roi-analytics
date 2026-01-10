65915770fcd96a28d5e425d4694a9b2379985382
somyasinha15 <93726730+somyasinha15@users.noreply.github.com>
2026-01-11 01:01:33 +0530

    Update 02_core_metrics.sql

--- sql/02_core_metrics.sql
+++ sql/02_core_metrics.sql
@@ -4,9 +4,24 @@ SELECT
   AS automation_coverage_pct
 FROM test_roi.test_cases;
 
+-- Manual vs Automation Execution Time
+SELECT
+    test_type,
+    AVG(manual_time_min) AS avg_manual_time,
+    AVG(automated_time_min) AS avg_automation_time
+FROM test_roi.test_cases
+GROUP BY test_type;
+
 -- Time Saved Per Test
 SELECT
   test_id,
   manual_time_min - automated_time_min AS time_saved_min
 FROM test_roi.test_cases
 WHERE test_type='Automation';
+
+-- Execution Volume by Release
+SELECT
+    release_version,
+    COUNT(*) AS total_executions
+FROM test_roi.test_runs
+GROUP BY release_version;
