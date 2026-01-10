3930c01cedccbd49c3963f9ce54bb5931af15459
somyasinha15 <93726730+somyasinha15@users.noreply.github.com>
2026-01-11 01:04:27 +0530

    Update 03_views.sql

--- sql/03_views.sql
+++ sql/03_views.sql
@@ -1,3 +1,4 @@
+-- Aggregated savings per release
 CREATE VIEW v_time_savings AS
 SELECT
   tr.release_version,
@@ -6,3 +7,14 @@ FROM test_roi.test_runs tr
 JOIN test_cases tc ON tr.test_id = tc.test_id
 WHERE tc.test_type='Automation'
 GROUP BY tr.release_version;
+
+-- Time saved per execution
+CREATE VIEW v_execution_time_saved AS
+SELECT
+    tr.execution_date,
+    tr.release_version,
+    tr.environment,
+    (tc.manual_time_min - tc.automated_time_min) AS time_saved_min
+FROM test_roi.test_runs tr
+JOIN test_roi.test_cases tc ON tr.test_id = tc.test_id
+WHERE tc.test_type = 'Automation';
